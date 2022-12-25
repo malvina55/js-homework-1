9d3b7774c8ed7c7425f56e437e6c4dc974beba50
MARGO <123@gfg>
2022-12-25 19:41:53 +0500

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+var imya = prompt('Введите имя');
+var age = prompt('Введите возраст');
+var plus = +prompt('5 + 6 = ');
+var minus = +prompt('15 - 5 = ');
+var umnoj = +prompt('5 * 6 = ');
+var del = +prompt('30 / 6 = ');
+var pros = +prompt('32 % 6 = ');
+alert('откройте консоль')
+
+
+
+console.log(imya);
+console.log(age);
+console.log('5 + 6 = ' + (5 + 6) + ' Ваш ответ: ' + plus);
+console.log('15 - 5 = ' + (15 - 5) + ' Ваш ответ: ' + minus);
+console.log('5 * 6 = ' + (5 * 6) + ' Ваш ответ: ' + umnoj);
+console.log('30 / 6 = ' + (30 / 6) + ' Ваш ответ: ' + del);
+console.log('32 % 6 = ' + (32 % 6) + ' Ваш ответ: ' + pros);
+
+// var x = +prompt('Введите  число')
+// var y = +prompt('Введите второе число')
+// var z = +prompt('Введите третье число')
+// var res = (x + y + z) / 3;
+
+// alert(res)
